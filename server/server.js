5f8a12ad1a426e2fb500eccd9fcc43f82b0ed8a1
garygreig <Gary@Gary.local>
2018-03-14 19:30:17 +0530

    Add tests for PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -83,14 +83,14 @@ app.patch('/todos/:id',(req, res) => {
    }
 
    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
-
      if(!todo)
      {
        return res.status(404).send();
      }
+
      res.send({todo});
    }).catch((e) => {
-   res.status(400).send();
+     res.status(400).send();
   })
 });
 
